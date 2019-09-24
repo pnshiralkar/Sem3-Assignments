b494fa8d83582d746780bce451a6a1c94940d85f
Pratham <pnshiralkar@gmail.com>
2019-09-24 15:52:33 +0530

    Completed PL a8 & a9

--- PL/a8_sparse.c
+++ PL/a8_sparse.c
@@ -1,169 +1,278 @@
 /*
- ============================================================================
- Name        : a8_sparse.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Assignment 8 - Sparse Matrix
- ============================================================================
- */
+------------------------------------------------------------
+Roll No.          : 23254
+Batch             : H10
+Problem Statement : Implement Sparse Matrix and perform the
+					following operations on it:
+					1) Addition
+					2) Simple Transpose
+					3) Fast Transpose
+------------------------------------------------------------
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
-#define R 50
-#define C 50
+#include <string.h>
 
-typedef struct s
+#define l 20
+
+typedef struct {
+	int row;
+	int col;
+	int val;
+}spMat;
+
+void getMat(spMat m[l])
 {
-	int row,column,value;
-}Sparse;
-
-int menu()
-{
-	int choice;
-	printf("Sparse Matrix Operations\n1. Accept matrix and convert into sparse matrix\n2. Addition of sparse matrix\n3. Simple transpose of sparse matrix\n4. Fast transpose of sparse matrix\n5. Convert sparse matrix into simple matrix\n6. Exit\n\nEnter your choice:- ");
-	scanf("%d",&choice);
-	return choice;
-}
-
-
-void accept(int mat[R][C],int *r,int *c,int *count, Sparse s[R])
-{
-	int i,j;
-	*count =1;
-	printf("Enter number of rows in the matrix:- ");
-	scanf("%d",r);
-	printf("Enter number of columns in the matrix:- ");
-	scanf("%d",c);
-	for(i=0;i<*r;i++)
-	{
-		for(j=0;j<*c;j++)
-		{
-			printf("Enter element [%d][%d]:- ",i,j);
-			scanf("%d",&mat[i][j]);
-			if(mat[i][j]!=0)
-				(*count)++;
-		}
-	}
-	convert(mat,*r,*c,*count,s);
-}
-
-void convert(int mat[R][C],int r,int c,int count, Sparse s[R])
-{
-	int i,j,k;
-	s[0].row=r;
-	s[0].column=c;
-	s[0].value=count-1;
-	k=1;
-	for(i=0;i<r;i++)
-	{
-		for(j=0;j<c;j++)
-		{
-			if(mat[i][j]!=0)
+	int i,j,t,temp,cnt=1;
+	printf("Enter elements one by one.\n");
+	for(i=0;i<m[0].row;i++)
+		for(j=0;j<m[0].col;j++)
+		{	scanf("%d",&temp);
+			if(temp!=0)
 			{
-				s[k].row=i;
-				s[k].column=j;
-				s[k].value=mat[i][j];
-				k++;
+				m[cnt].row = i;
+				m[cnt].col = j;
+				m[cnt].val = temp;
+				cnt++;
 			}
 		}
-	}
+	m[0].val = cnt-1;
 }
 
-void display(Sparse s[R],int count)
+void showMat(spMat m[l])
 {
-	int i,j;
-	printf("--------------------------------------------\n");
-	printf("Index\tRow\tColumn\tValue\n");
-	printf("--------------------------------------------\n");
-	for(i=0;i<count;i++)
-		printf("%d\t%d\t%d\t%d\n",i,s[i].row,s[i].column,s[i].value);
-	printf("--------------------------------------------\n");
-}
-
-void add(Sparse s1[R],Sparse s2[R],Sparse s3[R],int count)
-{
-	int i=1,j=1,k=1;
-	while(i<=s1[0].value && j<=s2[0].value)
+	int t;
+	printf("\n\tRow\tCol\tValue\n");
+	for(t=0;t<=m[0].val;t++)
 	{
-		if(s1[i].row==s2[j].row && s1[i].column == s2[j].column)
-		{
-			s3[k].row=s1[i].row;
-			s3[k].column=s1[i].column;
-			s3[k].value=s1[i].value + s2[j].value;
-			i++,j++,k++;
-		}
-		if(s1[i].row==s2[j].row && s1[i].column < s2[j].column)
-		{
-			s3[k].row=s1[i].row;
-			s3[k].column=s1[i].column;
-			s3[k].value=s1[i].value;
-			i++,k++;
-		}
-		if(s1[i].row==s2[j].row && s1[i].column > s2[j].column)
-		{
-			s3[k]=s2[j];
-			j++,k++;
-		}
-		if(s1[i].row < s2[j].row)
-		{
-			s3[k]=s1[i];
-			i++,k++;
-		}
-		if(s1[i].row > s2[j].row)
-		{
-			s3[k]=s2[j];
-			j++,k++;
-		}
-		while(i<=s1[0].value)
-		{
-			s3[k]=s1[i];
-			i++,k++;
-		}
-		while(j<=s2[0].value)
-		{
-			s3[k]=s2[j];
-			j++,k++;
-		}
-		s3[0].row=s1[0].row;
-		s3[0].column=s1[0].column;
-		s3[0].value=k;
+		printf("\t%d\t%d\t%d\n",m[t].row,m[t].col,m[t].val);
 	}
 }
 
-
-int main(void)
+void addMat(spMat m1[l],spMat m2[l],spMat m[l])
 {
-	int mat1[R][C],mat2[R][C];
-	int r1,c1,r2,c2;
-	int count;
-	Sparse s1[R],s2[R],s3[R];
-
-	while(1)
+	int i=1, j=1, k=1;
+	while(i<=m1[0].val && j<=m2[0].val)
 	{
-		switch(menu())
+		if(m1[i].row == m2[j].row && m1[i].col == m2[j].col)
 		{
-			case 1:
-				printf("Enter matrix 1: \n");
-				accept(mat1,&r1,&c1,&count,s1);
-				display(s1,count);
-				printf("Enter matrix 2: \n");
-				accept(mat2,&r2,&c2,&count,s2);
-				display(s2,count);
-				break;
-			case 2:
-				printf("\nMatrix 1 is: \n");
-				display(s1,count);
-				printf("\nMatrix 2 is: \n");
-				display(s2,count);
-				add(s1,s2,s3,count);
-				printf("\nAddition of matrices is: ");
-				display(s3,count);
-				break;
-			default:
-				return EXIT_SUCCESS;
+			m[k].row = m1[i].row;
+			m[k].col = m1[i].col;
+			m[k].val = m1[i].val + m2[j].val;
+			i++;
+			j++;
+			k++;
+		}
+		else if(m1[i].row == m2[j].row && m1[i].col < m2[j].col)
+		{
+			m[k] = m1[i];
+			i++;
+			k++;
+		}
+		else if(m1[i].row == m2[j].row && m1[i].col > m2[j].col)
+		{
+			m[k] = m2[j];
+			j++;
+			k++;
+		}
+		else if(m1[i].row < m2[j].row)
+		{
+			m[k] = m1[i];
+			i++;
+			k++;
+		}
+		else
+		{
+			m[k] = m2[j];
+			j++;
+			k++;
 		}
 	}
-	return EXIT_SUCCESS;
+	while(i<=m1[0].val)
+	{
+		m[k] = m1[i];
+		i++;
+		k++;
+	}
+	while(j<=m2[0].val)
+	{
+		m[k] = m2[j];
+		j++;
+		k++;
+	}
+	m[0].row = m1[0].row;
+	m[0].col = m1[0].col;
+	m[0].val = k-1;
+}
+
+void trans(spMat a[l], spMat b[l])
+{
+	int i,j,cb;
+	b[0].row = a[0].col;
+	b[0].col = a[0].row;
+	b[0].val = a[0].val;
+	if(a[0].val > 0)
+	{
+		cb = 1;
+		for(i=0; i<a[0].col; i++)
+			for(j=1; j<=a[0].val; j++)
+				if(a[j].col == i)
+				{
+					b[cb].row = a[j].col;
+					b[cb].col = a[j].row;
+					b[cb].val = a[j].val;
+					cb++;
+				}
+	}
+}
+
+void fasttrans(spMat a[l], spMat b[l])
+{
+	int row_terms[l], start_pos[l];
+	int i, j, num_cols = a[0].col, num_terms = a[0].val;
+	b[0].row = num_cols;
+	b[0].col = a[0].row;
+	b[0].val = num_terms;
+
+	if(num_terms > 0)
+	{
+		for(i=0; i<num_cols; i++)
+			row_terms[i] = 0;
+		for(i=1; i<=num_terms; i++)
+			row_terms[a[i].col]++;
+        start_pos[0] = 1;
+        for(i=1; i<num_cols; i++)
+            start_pos[i] = start_pos[i-1] + row_terms[i-1];
+		for(i=1; i<=num_terms; i++)
+		{
+			j = start_pos[a[i].col]++;
+			b[j].row = a[i].col;
+			b[j].col = a[i].row;
+			b[j].val = a[i].val;
+		}
+	}
+}
+
+void MatDisplay(spMat m[l])
+{
+    int i, j, t=1, a;
+
+    printf("\t");
+    for(i=0; i<m[0].row; i++)
+    {
+        for(j=0; j<m[0].col; j++)
+        {
+            if(i==m[t].row && j==m[t].col)
+            {
+                a=m[t].val;
+                t++;
+            }
+            else
+                a=0;
+            printf(" %d  ", a);
+        }
+        printf("\n\t");
+    }
+}
+
+int main()
+{
+	int ch,t=0;
+	spMat m1[l],m2[l],m3[l];
+
+	do
+	{
+		printf("\n Menu :\n1. Input Matrix.\n2. Display Matrix.\n3. Add Matrices.\n4. Simple Transpose.\n5. Fast Transpose.\n6. Exit.\nEnter your choice : ");
+		scanf("%d",&ch);
+		switch(ch)
+		{
+			case 1:	printf("\nSparse Matrix 1:\n");
+					printf("Enter no. of rows : ");
+					scanf("%d", &m1[0].row);
+					printf("Enter no. of columns : ");
+					scanf("%d", &m1[0].col);
+					getMat(m1);
+					t=1;
+					break;
+
+			case 2:	if(t)
+					{	printf("\nSHOW MATRIX:\nEntered Sparse Matrix is :-\n");
+						showMat(m1);
+						printf("\nIn Matrix Form :\n");
+						MatDisplay(m1);
+					}
+					else
+						printf("\nEnter Matrix First..!!\n");
+					break;
+
+			case 3:	if(!t)
+					{
+						printf("\nSparse Matrix 1:\n");
+						printf("Enter no. of rows : ");
+						scanf("%d", &m1[0].row);
+						printf("Enter no. of columns : ");
+						scanf("%d", &m1[0].col);
+						getMat(m1);
+						t=1;
+					}
+					printf("\nEnter another Matrix to ADD :");
+					printf("\nSparse Matrix 2:\n");
+					printf("Enter no. of rows : ");
+					scanf("%d", &m2[0].row);
+					printf("Enter no. of columns : ");
+					scanf("%d", &m2[0].col);
+					getMat(&m2);
+
+					printf("\nADDITION:\n");
+					if(m1[0].row==m2[0].row && m1[0].col==m2[0].col)
+					{	addMat(m1,m2,m3);
+						printf("\nSparse Matrix 1:");
+						showMat(m1);
+						printf("\nSparse Matrix 2:");
+						showMat(m2);
+						printf("\nSum :");
+						showMat(m3);
+
+						printf("\nSparse Matrix 1:\n");
+						MatDisplay(m1);
+						printf("\nSparse Matrix 2:\n");
+						MatDisplay(m2);
+						printf("\nSum :\n");
+						MatDisplay(m3);
+					}
+					else
+						printf("\nCannot Add Matrices\n");
+					break;
+
+			case 4:	if(t)
+					{	printf("\nSIMPLE TRANSPOSE:\n");
+						trans(m1,m3);
+						printf("\nSparse Matrix 1:");
+						showMat(m1);
+						printf("\nTranspose of Sparse Matrix 1 :");
+						showMat(m3);
+					}
+					else
+						printf("\nEnter Matrix First..!!\n");
+					break;
+
+			case 5:	if(t)
+					{	printf("\nFAST TRANSPOSE:\n");
+						fasttrans(m1,m3);
+						printf("\nSparse Matrix 1:");
+						showMat(m1);
+						printf("\nTranspose of Sparse Matrix 1 :");
+						showMat(m3);
+					}
+					else
+						printf("\nEnter Matrix First..!!\n");
+					break;
+
+			default: return 0;
+		}
+	}while(ch!=6);
+
+    return 0;
 }
 
--- PL/a9LL.c
+++ PL/a9LL.c
@@ -11,6 +11,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define debug(n) printf("debug%d", n)
+
 struct Nodep;
 
 typedef struct Nodep{
@@ -41,6 +43,7 @@ void insert(LinkedList *ll,int n, int p)
 		}
 	if(p==0)
 	{
+
 		if(ni==NULL)
 		{
 			new->nxt = NULL;
@@ -55,42 +58,76 @@ void insert(LinkedList *ll,int n, int p)
 	}
 	else
 	{
-		for(;i<p-1;i++){
-			if(ni==NULL)
-				printf("Invalid position!");
+		for(i=0;i<p-1;i++){
+			if(!ni){
+				printf("Invalid position!\n");
+				return;
+			}
 			ni = ni->nxt;
 		}
 		new->nxt = ni->nxt;
-		if(new->nxt==NULL)
+		if(!(new->nxt))
 			ll->end=new;
-//		if(new->nxt==NULL)
-//			printf("%d is last", n);
 		ni->nxt = new;
 	}
 }
 
-void delLL(LinkedList *ll, int p)
+void insertLast(LinkedList *ll,int n)
 {
-	Node *c;
-	int i;
-	if(p==0)
+	Node *ni=ll->start,*new=(Node*)malloc(sizeof(Node));
+
+	new->val = n;
+	if(ni==NULL)
 	{
-		c=ll->start;
-		ll->start = c->nxt;
-		free(c);
+		new->nxt = NULL;
+		ll->start=new;
+		ll->end = new;
 	}
 	else
 	{
-		for(i=0,c=ll->start;i<p-1;i++,c=c->nxt)
-			if(c->nxt==NULL)
-				printf("Invalid value!");
-		free(c->nxt);
-		c->nxt = c->nxt->nxt;
+		for(;ni->nxt;ni = ni->nxt);
+		new->nxt = ni->nxt;
+		if(!(new->nxt))
+			ll->end=new;
+		ni->nxt = new;
+	}
+}
+
+void delLL(LinkedList *ll, int v)
+{
+	Node *c,*p;
+	int i;
+	if(!(ll->start))
+	{
+		printf("LinkedList is empty!\n");
+		return;
+	}
+	for(i=0,c=ll->start;;i++,c=c->nxt)
+	{
+		if(!(c->nxt)){
+			printf("Value not found in the LinkedList!!\n");
+			return;
+		}
+		if(c->val==v)
+		{
+
+			if(c==ll->start){
+				ll->start = c->nxt;
+				free(c);
+			}
+			else{
+				free(p->nxt);
+				p->nxt = p->nxt->nxt;
+			}
+			return;
+		}
+		p=c;
 	}
 }
 
 void dispLL(LinkedList ll)
 {
+	printf("LinkedList is : ");
 	Node *c=ll.start;
 	while(c!=NULL)
 	{
@@ -100,15 +137,96 @@ void dispLL(LinkedList ll)
 	printf("\n");
 }
 
+void revLL(LinkedList *ll)
+{
+	Node *a=ll->start,*b,*c;
+	if(a){
+		b=a->nxt;
+		a->nxt=NULL;
+	}
+	while(b->nxt)
+	{
+		c=b->nxt;
+		b->nxt=a;
+		a=b;
+		b=c;
+	}
+	b->nxt=a;
+	ll->start = b;
+}
+
+int menu()
+{
+	int r;
+	printf("\n\n***LinkedList in C***\n\n1. Create LinkedList\n2. Insert\n3. Insert at position\n4. Delete a node\n5. Display\n6. Display reverse\n7. Revert LinkedList\n8. Exit\n\tYour choice : ");
+	scanf("%d", &r);
+	return r;
+}
+
 int main(void) {
 	LinkedList ll;
-	init(&ll);
-	insert(&ll,22,0);
-	insert(&ll,6,1);
-	dispLL(ll);
-	delLL(&ll, 1);
-	dispLL(ll);
+	int inited=0,v,p;
+//	init(&ll);
+//	insert(&ll,22,0);
+//	insert(&ll,6,100);
+//	insert(&ll,66,9);
+//	insert(&ll,29,1);
+//	dispLL(ll);
+//	delLL(&ll, 1);
+//	dispLL(ll);
+//	revLL(&ll);
+//	dispLL(ll);
+//	revLL(&ll);
+//	dispLL(ll);
+	while(1)
+	{
+		switch(menu())
+		{
+		case 1:
+			init(&ll);
+			inited=1;
+			printf("LinkedList created successfully!");
+			break;
+		case 2:
+			if(!inited){printf("Create a LinkedList first!"); break;}
+			printf("Enter value to insert : ");
+			scanf("%d", &v);
+			insertLast(&ll, v);
+			break;
+		case 3:
+			if(!inited){printf("Create a LinkedList first!"); break;}
+			printf("Enter value to insert : ");
+			scanf("%d", &v);
+			printf("Enter position at which you want to insert : ");
+			scanf("%d", &p);
+			insert(&ll, v, p);
+			break;
+		case 4:
+			if(!inited){printf("Create a LinkedList first!"); break;}
+			printf("Enter value to delete : ");
+			scanf("%d", &v);
+			delLL(&ll, v);
+			break;
+		case 5:
+			if(!inited){printf("Create a LinkedList first!"); break;}
+			dispLL(ll);
+			break;
+		case 6:
+			if(!inited){printf("Create a LinkedList first!"); break;}
+			revLL(&ll);
+			dispLL(ll);
+			revLL(&ll);
+			break;
+		case 7:
+			if(!inited){printf("Create a LinkedList first!"); break;}
+			revLL(&ll);
+			printf("LinkedList reverted successfully!");
+			break;
+		default:
+			return 0;
+		}
+	}
+
 
 	return EXIT_SUCCESS;
 }
-
